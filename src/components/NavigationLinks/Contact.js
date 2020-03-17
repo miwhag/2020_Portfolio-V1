5856113d741fe114f8ce83d25c241f4a1f6fc9ad
github username <github email address>
2020-03-16 22:01:08 -0600

    added contact me page with setup to express

--- src/components/NavigationLinks/Contact.js
+++ src/components/NavigationLinks/Contact.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../ContactPage/Contact.css'
 import Footer from '../Footer'
+import ContactMe from '../ContactPage/ContactMe'
 
 export default function Contact() {
     return (
         <div>
-            <h1>Contact Page</h1>
-            <main className="Contact-main-container"></main>
+            <main className="Contact-main-container">
+            <ContactMe />
+            </main>
+
             <Footer/>
         </div>
     )
